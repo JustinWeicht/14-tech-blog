1efa6ba3975a0f4385520b2934f1d04e474b0b87
JustinWeicht <justinweicht11@gmail.com>
2021-09-13 14:24:41 -0400

    created /ulit files and server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const sequelize = require('./config/connection');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const controller = require('./controllers');
+const path = require('path');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'mySecret2',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db:sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', controller);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
